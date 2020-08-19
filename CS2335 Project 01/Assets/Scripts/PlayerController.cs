c0d101236d2526ac35dac50d08caecf63e0d7357
Valerie Vasquez <valerie.vasquez23@gmail.com>
2020-08-19 13:18:41 -0500

    initial commit

---
+++ CS2335 Project 01/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//player movement
+public class PlayerController : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
